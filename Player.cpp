dd4e7d6bfa824c77dd7e081b32a726d4bdde2e15
InoueSota <k022g0045@g.neec.ac.jp>
2023-04-17 16:33:29 +0900

    メンバ変数の追加

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,19 @@
 #include "Player.h"
+
+
+
+Player::Player() {}
+
+Player::~Player() {}
+
+void Player::Initialize() {
+
+}
+
+void Player::Update() {
+
+}
+
+void Player::Draw() {
+
+}
--- Player.h
+++ Player.h
@@ -1,15 +1,36 @@
 #pragma once
+#include "Model.h"
+#include "WorldTransform.h"
 
 
 
 class Player {
+
 public:
 	Player();
 	~Player();
 
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
 private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
 };
 
-Player::Player() {}
-
-Player::~Player() {}
